f9cda308ac85a6feaf468e09372a7214beff0686
Nutan Singh <nutansingh159@gmail.com>
2024-07-28 21:41:48 +0530

    added loading in trending tv page

--- src/pages/trending_tv_page.js
+++ src/pages/trending_tv_page.js
@@ -5,9 +5,11 @@ import TvCard from '../components/tv_card';
 function TrendingTvPage() {
 
   const [tvShows, setTvShows] = useState([]);
+  const [isMovieLoading, setisMovieLoading] = useState(true);
 
   const getTrendingTv = async () => {
     const result = await fetchTrendingTv();
+    setisMovieLoading(false);
     setTvShows(result);
   };
 
@@ -20,13 +22,27 @@ function TrendingTvPage() {
       <h1 className='my-4'>Trending TV</h1>
       <div className="row">
         {
-          tvShows.map((tvshow) => {
-            return <TvCard data={tvshow} key={tvshow['id']} />
-          })
+          isMovieLoading
+            ? [1, 2, 3, 4, 5, 6].map((e) => {
+              return (
+                <div className="col-md-6 col-lg-4 my-2">
+                  <div
+                    style={{
+                      height: '300px',
+                      background: 'lightgray',
+                    }}
+                  >
+                  </div>
+                </div>
+              );
+            })
+            : tvShows.map((tvshow) => {
+              return <TvCard data={tvshow} key={tvshow['id']} />
+            })
         }
       </div>
-    </div>
-  )
+    </div >
+  );
 }
 
 export default TrendingTvPage;
